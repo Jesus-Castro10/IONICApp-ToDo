5cbf645e079e1b4799dba491bfc37c708c7314df
Jesus Castro <jesusdavidcastror30@gmail.com>
2025-03-16 18:41:26 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TallerToDo',
+  webDir: 'www'
+};
+
+export default config;
